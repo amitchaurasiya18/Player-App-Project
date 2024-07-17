6c1bf437604aaebcbe71a677a2a5429d9b822408
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-17 10:10:22 +0530

    Initial commit

---
+++ Player.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerAppProject
+{
+    internal class Player
+    {
+        private readonly int _playerId;
+        private readonly string _playerName;
+        private readonly int _playerAge;
+        public const int DEFAULT_AGE = 0;
+        public static int maximumAge = DEFAULT_AGE;
+
+        public Player(int playerId, string playerName) : this(playerId, playerName, DEFAULT_AGE)
+        {
+        }
+
+        public Player(int playerId, string playerName, int playerAge)
+        {
+            _playerId = playerId;
+            _playerName = playerName;
+            _playerAge = playerAge;
+        }
+
+        public int GetPlayerId()
+        {
+            return _playerId;
+        }
+
+        public string GetPlayerName()
+        {
+            return _playerName;
+        }
+
+        public int GetPlayerAge()
+        {
+            return _playerAge;
+        }
+
+        public Player ReturnWhoIsElder(Player[] players)
+        {
+            Player player1 = null;
+            foreach (Player player in players)
+            {
+                if(player._playerAge > DEFAULT_AGE)
+                {
+                    maximumAge = player._playerAge;
+                    player1 = player;
+                }
+            }
+
+            return player1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayerAppProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player[] players = new Player[5]
+            {
+                new Player(101,"Harsh",25),
+                new Player(102,"Abhishek",35),
+                new Player(103,"Amit", 22),
+                new Player(104, "Riya",24),
+                new Player(105, "Ram", 34)
+            };
+
+            bool continueProgram = true;
+
+            while (continueProgram)
+            {
+                Console.Write("Enter the Player Id to perform actions (or enter 0 to exit): ");
+                int playerId = int.Parse(Console.ReadLine());
+
+                if (playerId == 0)
+                {
+                    continueProgram = false;
+                    break;
+                }
+
+                Player selectedPlayer = null;
+
+                foreach (Player player in players)
+                {
+                    if (player.GetPlayerId() == playerId)
+                    {
+                        selectedPlayer = player;
+                        break;
+                    }
+                }
+
+                if (selectedPlayer != null)
+                {
+                    bool continueTransactions = true;
+                    while (continueTransactions)
+                    {
+                        Console.WriteLine("\n------Select transaction:------\nPress 1 for Player Name\nPress 2 for Player Age\nPress 3 for Oldest Player\nPress 4 to exit");
+                        int selectTransaction = int.Parse(Console.ReadLine());
+
+                        switch (selectTransaction)
+                        {
+                            case 1:
+                                Console.WriteLine($"The Player's Name : {selectedPlayer.GetPlayerName()}");
+                                break;
+                            case 2:
+                                Console.WriteLine($"The Player's Age : {selectedPlayer.GetPlayerAge()}");
+                                break;
+                            case 3:
+                                Player elderPlayer = selectedPlayer.ReturnWhoIsElder(players);
+                                Console.WriteLine($"The Oldest Player's Id : {elderPlayer.GetPlayerId()}");
+                                Console.WriteLine($"The Oldest Player's Name : {elderPlayer.GetPlayerName()}");
+                                Console.WriteLine($"The Oldest Player's Age : {elderPlayer.GetPlayerAge()}");
+                                break;
+                            case 4:
+                                continueTransactions = false;
+                                break;
+                            default:
+                                Console.WriteLine("Select Transaction again...");
+                                break;
+                        }
+                    }
+                }
+                else
+                    Console.WriteLine("Account not found. Please try again.");
+            }
+        }
+    }
+}
